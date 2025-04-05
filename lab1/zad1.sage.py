f95477d0159b8e6b703924924c550bc4c20ba7b7
all0ver <mateusz.karpinski29@gmail.com>
2025-04-05 21:11:43 +0000

    pd, lab1, diffi-hellman

--- lab1/zad1.sage.py
+++ lab1/zad1.sage.py
@@ -16,15 +16,73 @@ while not is_prime(p):
     n = next_prime(n)
     p = _sage_const_2 *n+_sage_const_1 
 
-print(n)
-print(p)
+print("\nWyznaczenie liczby pierwszej n oraz liczby pierwszej p = 2n+1:\n")
+print("\nOtrzymana liczban: ", n)
+print("\nOtrzymana liczba p: ", p)
 
 # inicjacja ciała F
+print("\nInicjalizacja ciala F(p)\n")
 F = GF(p)
 
-g = F.random_element()
+# znalezeinie takeigo g (generatora), że #<g> = n
+while True:
+    g = F.random_element()
+    if g.multiplicative_order() == n:
+        break
 
-print(g.multiplicative_order())
+print("\nZnalezienie generatora g, dla ktorego #<g> = n: ", g)
+# print("\ng:")
+# print(g.multiplicative_order())
+
+# generowanie kluczy prywatnych A, B, C
 
 xa = random.randint(_sage_const_1 , n-_sage_const_1 )
+xb = random.randint(_sage_const_1 , n-_sage_const_1 )
+xc = random.randint(_sage_const_1 , n-_sage_const_1 )
+print("\nGenerowanie kluczy prytwatnych A, B, C: xa: ", xa, " xb: ", xb, " xc: ", xc)
+
+# generowanie kluczy publicznych A, B, C
+
+KA = (g**xa)
+KB = (g**xb)
+KC = (g**xc)
+print("\nGenerowanie kluczy publicznych: \nKA: ", KA, "\nKB: ", KB, "\nKC: ", KC)
+
+# pierwsza wymiana kluczy publicznych
+
+print("\nPierwsza wymiana kluczy publicznych:\n")
+# a oblicza KCA
+KCA = (KC**xa)
+# b oblicza KAB
+KAB = (KA**xb)
+# c oblicza KBC
+KBC = (KB**xc)
+print("\nObliczone klucze po pierwszej wymianie:")
+print("\nKlucz obliczony przez A: (KC^xa): ", KCA)
+print("\nKlucz obliczony przez B: (KA^xc): ", KAB)
+print("\nKlucz obliczony przez C: (KB^xc): ", KBC)
+
+# druga wymiana kluczy publicznych
+# obliczenie kluczy sesji
+
+# a oblicza K_BC^xa
+K_BCA = (KBC**xa)
+# b oblicza K_CA^xb
+K_CAB = (KCA**xb)
+# c oblicza K_AB^xc
+K_ABC = (KAB**xc)
+
+print("\nObliczone klucze po drugiej wymianie:")
+print("\nKlucz obliczony przez A: (KBC^xa): ", K_BCA)
+print("\nKlucz obliczony przez B: (KCA^xb): ", K_CAB)   
+print("\nKlucz obliczony przez C: (KAB^xc): ", K_ABC)
+
+print("\nKlucze sesyjne:\n")
+print("\nKlucz obliczny przez A: ", K_BCA)
+print("\nKlucz obliczny przez B: ", K_CAB)
+print("\nKlucz obliczny przez C: ", K_ABC)
+
+if K_BCA == K_CAB and K_CAB == K_ABC:
+    print("\nKlucze sesji są takie same")
+
 
